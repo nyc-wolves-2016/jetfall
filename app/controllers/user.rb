a7d0c18befc01e9cb03a1555edb453b1aed067be
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-21 17:11:08 -0400

    Edit routes for logging in/out and protecting routes.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,10 +1,15 @@
 get '/register' do
-  erb :'users/new'
+  if logged_in?
+    redirect '/'
+  else
+    erb :'users/new'
+  end
 end
 
 post '/register' do
   user = User.new(params[:user])
   if user.save
+    session[:user_id] = user.id
     redirect '/'
   else
     @errors = user.errors.full_messages
@@ -13,9 +18,27 @@ post '/register' do
 end
 
 get '/login' do
+  if logged_in?
+    redirect '/'
+  else
+    erb :'users/login'
+  end
+end
 
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @errors = ["Invalid email or password"]
+    erb :'users/login'
+  end
 end
 
 get '/logout' do
-
+  if logged_in?
+    session.clear
+  end
+    redirect '/'
 end
