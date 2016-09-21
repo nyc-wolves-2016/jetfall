5a49f869bd168e20760ed7b6ecbe593c78cab86d
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-21 11:17:49 -0400

    changed post migration to questions

--- db/migrate/20160921105301_create_posts.rb
+++
@@ -1,4 +0,0 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20160921105311_create_answers.rb
+++ db/migrate/20160921105311_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answer do |t|
+      t.text  :body, null: false
+      t.references  :user
+      t.references :question
+
+      t.timestamps(null: false)
+    end
   end
 end
