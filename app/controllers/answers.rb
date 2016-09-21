2e42bf1711f27cd787c4767d71f7d336a874a5fb
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-21 18:30:33 -0400

    fix changes and added user log in method to form

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,11 +1,14 @@
 post '/answers' do
   answer = Answer.new(params[:answer])
+  @question = Question.find(answer.question_id)
+  @author = User.find(@question.user_id)
+  @answers = Answer.where(question_id: @question.id)
   if answer.save
     answer.user = current_user
     answer.save
     redirect "/questions/#{answer.question_id}"
   else
-    @errors = user.errors.full_messages
-    redirect "questions/#{answer.question_id}"
+    @errors = answer.errors.full_messages
+    erb :'questions/show'
   end
 end
