598f61fd88abc324b9299845c2fead994f8d99f9
Jason Wong <jason.wong411@gmail.com>
2016-09-22 11:03:40 -0400

    Add best function; best comment will show at top, comments sorted oldest to newest

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -5,10 +5,21 @@ end
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @author = User.find(@question.user_id)
-  @answers = Answer.where(question_id: @question.id)
-  @question_comments = @question.comments
-  
+  @answers = @question.answers
+  @question_comments = @question.comments.order(created_at: :asc)
+  if @question.answer_id != nil
+    @best_answer = Answer.find(@question.answer_id)
+    @answers = @answers.where.not(id: @question.answer_id)
+  end
+
   erb :'questions/show'
+end
+
+post '/question/:id/best' do
+  question = Question.find(params[:id])
+  question.update_attribute(:answer_id, params[:answer_id])
+
+  redirect "/questions/#{question.id}"
 end
 
 post '/questions' do
