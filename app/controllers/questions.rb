18946237cdbd92ca87402b99c92d78c54bdfad45
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-22 11:24:05 -0400

    implement voting system for questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -21,23 +21,3 @@ post '/questions' do
     erb :'questions/new'
   end
 end
-
-get '/questions/:id/upvote' do
-  user_upvoted = Vote.find_by(user_id: current_user.id, votable_id: params[:id])
-  if !user_upvoted
-    question = Question.find(params[:id])
-    question_vote = Vote.create(value: 1, user_id: current_user.id, votable: question)
-    question.points
-    redirect "/questions/#{question.id}"
-  end
-    redirect "/questions/#{params[:id]}"
-end
-
-get '/questions/:id/downvote' do
-  question = Question.find(params[:id])
-  question_vote = Vote.new(value: -1, user_id: current_user.id)
-  question_vote.votable = question
-  question_vote.save
-  question.points
-  redirect "/questions/#{question.id}"
-end
