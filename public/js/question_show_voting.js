7a1e8763cc2e21e74b2d77d3c49b8da8bc1a8886
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-23 09:58:03 -0400

    merge conflicts

--- public/js/question_show_voting.js
+++ public/js/question_show_voting.js
@@ -2,7 +2,7 @@ $(function() {
   // question upvote
   $('.question-container').on('click', '.upvote', function(event) {
       event.preventDefault();
-      console.log('hey you clicked on me');
+      // console.log('hey you clicked on me');
 
       // console.log($(this).attr('href'));
       var question_id = $(this).parent().find('span').attr('id');
@@ -14,6 +14,41 @@ $(function() {
        .done(function(response) {
         $('#'+question_id).text(response+' points');
       });
+  });
 
+  // question downvote
+  $('.question-container').on('click', '.downvote', function(event) {
+    event.preventDefault();
+    // console.log('hey you clicked on me');
+
+    var question_id = $(this).parent().find('span').attr('id');
+    // console.log(question_id);
+    $.ajax({
+      method: 'GET',
+      url: $(this).attr('href')
+    })
+     .done(function(response) {
+       $('#'+question_id).text(response+ ' points');
+     });
+  });
+
+  // question comment upvote
+  $('.question_comments').on('click', '.upvote', function(event) {
+    event.preventDefault();
+    console.log('hey you clicked me');
+    // console.log($(this).parent().parent().find('span'));
+    console.log($(this).closest('li').attr('id'));
+    var comment_id = $(this).parent().parent().find('span').attr('id');
+    // console.log(comment_id);
+    $.ajax({
+      method: 'GET',
+      url: $(this).attr('href')
+    })
+     .done(function(response) {
+      //  console.log('hey are you there?');
+      //  console.log(response);
+      // console.log($('#'+comment_id).closest('span').val());
+      $('#'+comment_id).text(response);
+     })
   });
 });
