fe855a9109689cb9ab9d339c15c06353f9ac8a9f
Edwin Carbajal & Jason Wong & Tyler Sharp <pair+edwincarbajal.jwong411.tylervsharp@devbootcamp.com>
2016-09-23 10:28:37 -0400

    final commits

--- public/js/question_show_voting.js
+++ public/js/question_show_voting.js
@@ -2,16 +2,15 @@ $(function() {
   // question upvote
   $('.question-container').on('click', '.upvote', function(event) {
       event.preventDefault();
-      // console.log('hey you clicked on me');
 
-      // console.log($(this).attr('href'));
-      var question_id = $(this).parent().find('span').attr('id');
+      var question_id = $(this).next().next().attr('id');
 
       $.ajax({
         method: 'GET',
         url: $(this).attr('href')
       })
        .done(function(response) {
+        //  console.log($('#'+question_id));
         $('#'+question_id).text(response+' points');
       });
   });
@@ -19,10 +18,11 @@ $(function() {
   // question downvote
   $('.question-container').on('click', '.downvote', function(event) {
     event.preventDefault();
-    // console.log('hey you clicked on me');
+    console.log('hey you clicked on me');
 
-    var question_id = $(this).parent().find('span').attr('id');
-    // console.log(question_id);
+    var question_id = $(this).prev().prev().attr('id');
+    console.log(question_id);
+
     $.ajax({
       method: 'GET',
       url: $(this).attr('href')
@@ -36,10 +36,10 @@ $(function() {
   $('.question_comments').on('click', '.upvote', function(event) {
     event.preventDefault();
     console.log('hey you clicked me');
-    // console.log($(this).parent().parent().find('span'));
-    console.log($(this).closest('li').attr('id'));
-    var comment_id = $(this).parent().parent().find('span').attr('id');
-    // console.log(comment_id);
+
+    var comment_id = $(this).next().attr('id');
+
+    console.log(comment_id);
     $.ajax({
       method: 'GET',
       url: $(this).attr('href')
@@ -48,7 +48,8 @@ $(function() {
       //  console.log('hey are you there?');
       //  console.log(response);
       // console.log($('#'+comment_id).closest('span').val());
-      $('#'+comment_id).text(response);
+      // console.log($(event.target).next());
+      $(event.target).next().text(response+ ' points');
      })
   });
 });
